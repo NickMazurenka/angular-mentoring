340fa74756bca5402752fc9fe2320b0d6fabebe5
SexPixels <nik.mazurenko@gmail.com>
2017-11-20 17:02:20 +0300

    Add fake components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoFakeComponent } from './header/logo-fake/logo-fake.component';
+import { LogoComponent } from './header/logo/logo.component';
+import { FooterComponent } from './footer/footer.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoFakeComponent,
+    LogoComponent,
+    FooterComponent,
+    CoursesComponent,
+    CourseDetailsComponent
   ],
   imports: [
     BrowserModule,
