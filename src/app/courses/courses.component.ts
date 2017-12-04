08aaf81407f5bfdd0058f55a698d4810f069d140
Mikalai Mazurenka <mikalai_mazurenka@epam.com>
2017-12-04 12:03:09 +0300

    Add courses list in init

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -7,26 +7,29 @@ import { ICourseDetails } from './course-details/course-details.model';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-  courses: ICourseDetails[] = [{
-    id: 1,
-    description: 'Awesome course one',
-    date: '24 Nov 17',
-    duration: '1h 27 min'
-  }, {
-    id: 2,
-    description: 'Awesome course two',
-    date: '24 Nov 17',
-    duration: '1h 27 min'
-  }, {
-    id: 3,
-    description: 'Awesome course three',
-    date: '24 Nov 17',
-    duration: '1h 27 min'
-  }];
+  courses: ICourseDetails[];
 
-  constructor() { }
+  constructor() {
+    this.courses = [];
+  }
 
   ngOnInit() {
+    this.courses.push({
+      id: 1,
+      description: 'Awesome course one',
+      date: '24 Nov 17',
+      duration: '1h 27 min'
+    }, {
+      id: 2,
+      description: 'Awesome course two',
+      date: '24 Nov 17',
+      duration: '1h 27 min'
+    }, {
+      id: 3,
+      description: 'Awesome course three',
+      date: '24 Nov 17',
+      duration: '1h 27 min'
+    });
   }
 
   find(pattern: string) {
