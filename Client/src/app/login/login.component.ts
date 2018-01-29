81817c8ee030d8d4130fafe3f09d8a14cb822f14
SexPixels <nik.mazurenko@gmail.com>
2018-01-30 00:46:01 +0300

    Add login form

--- Client/src/app/login/login.component.ts
+++ Client/src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { Observer } from 'rxjs/Observer';
 import { Subscription } from 'rxjs/Subscription';
 import { Location } from '@angular/common';
 import { AuthService } from '../shared-services/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -13,25 +14,48 @@ import { AuthService } from '../shared-services/auth.service';
 })
 export class LoginComponent implements OnDestroy {
 
-  public userName: string;
-  public userPassword: string;
+  loginForm: FormGroup;
 
   private _loginSubscription: Subscription;
 
   constructor(
     private auth: AuthService,
     private router: Router,
+    private formBuilder: FormBuilder,
     private locationService: Location) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
   }
 
   onSubmit() {
-    this._loginSubscription = this.auth.logIn(this.userName, this.userPassword).subscribe(() => {
-      this.router.navigate(['']);
-    });
+    this._loginSubscription = this.auth.logIn(
+      this.loginForm.get('username').value,
+      this.loginForm.get('password').value).subscribe(() => {
+        this.router.navigate(['']);
+      });
   }
 
   onCancelClick() {
     this.locationService.back();
+  }
+
+  showControlError(controlName: string): boolean {
+    const control = this.loginForm.get(controlName);
+    return control.invalid && control.touched;
+  }
+
+  getControlError(controlName: string): string {
+    const control = this.loginForm.get(controlName);
+    if (control.errors.required) {
+      return `Please specify ${controlName}`;
+    }
+  }
+
+  validateAllFormFields(formGroup: FormGroup) {
+    this.loginForm.get('username').markAsTouched({ onlySelf: true });
+    this.loginForm.get('password').markAsTouched({ onlySelf: true });
   }
 
   ngOnDestroy() {
