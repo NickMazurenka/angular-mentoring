b51ee0eb161426353287643c78bf4f04cd57f5a6
SexPixels <nik.mazurenko@gmail.com>
2018-02-20 01:25:05 +0300

    Add mock service to add course component

--- Client/src/app/courses/add-course/add-course.component.spec.ts
+++ Client/src/app/courses/add-course/add-course.component.spec.ts
@@ -1,7 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, Store } from '@ngrx/store';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import * as AuthorsActions from '../store/authors.actions';
 import { AddCourseComponent } from './add-course.component';
 import { CourseAuthorsSelectorComponent } from './course-authors-selector/course-authors-selector.component';
 import { CourseDateInputComponent } from './course-date-input/course-date-input.component';
@@ -10,11 +12,11 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { CourseDurationPipe } from '../course-details/course-duration.pipe';
 import { CourseReducer } from '../store/course.reducer';
 import { AuthorsReducer } from '../store/authors.reducer';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AddCourseComponent', () => {
   let component: AddCourseComponent;
   let fixture: ComponentFixture<AddCourseComponent>;
+  let store: Store<any>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,7 +25,7 @@ describe('AddCourseComponent', () => {
         FormsModule,
         ReactiveFormsModule,
         RouterTestingModule,
-        StoreModule.forRoot({ courses: CourseReducer, AuthorsReducer })
+        StoreModule.forRoot({ courses: CourseReducer, authors: AuthorsReducer })
       ],
       declarations: [
         AddCourseComponent,
@@ -38,10 +40,24 @@ describe('AddCourseComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AddCourseComponent);
     component = fixture.componentInstance;
+    store = fixture.debugElement.injector.get(Store);
+    store.dispatch(new AuthorsActions.GetAuthorListRequestSuccess([{
+      id: 0,
+      firstName: 'Wat',
+      lastName: 'Dat'
+    }]));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have authors list', () => {
+    expect(component.authors).toEqual([{
+      id: 0,
+      firstName: 'Wat',
+      lastName: 'Dat'
+    }]);
   });
 });
